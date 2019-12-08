26d7249ff27f9eb9b5ccbd91cb1cfb92ee9a2330
Louis Huang <louisopen@gmail.com>
2019-12-08 13:02:11 +0800

    update

---
+++ autorun.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#This bash called by autoboot.desktop or rootcron   #autoboot.desktop存放到/home/pi/.config/autostart/
+#chmod =777 autoboot.desktop
+#sudo nano .bashrc   	#最後行添加 sh autorun.sh  同時(近端/遠端)啟動
+_IP=$(hostname -I) || true
+if [ "$_IP" ]; then
+  printf "My IP address is %s\n" "$_IP"
+fi
+
+#su pi
+source_dir=$PWD
+jump_dir=/home/pi
+cd $jump_dir
+#tr -d "\r" < autorun.sh > newname.sh    #if you can't cd to .... just do this command. #置入新系統時若發生此訊息
+#tr -d "\r" < autorun.sh > newname.sh    #if you got ...Syntax error: end of file unexpected (expecting "then")
+
+sudo python $jump_dir/stats.py &
+#python $jump_dir/Function.py
+#python $jump_dir/Program_eflash.
+#sudo python3 $jump_dir/rpi_rf/rpi-rf_receive &
+sudo python3 $jump_dir/rpi_rf/rpi-rf_receive
+
+cd $source_dir
+exit 0
